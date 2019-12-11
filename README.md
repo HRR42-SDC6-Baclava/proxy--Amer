13cd136e6dcad52c39ba1a0ea064efa837757ac4
Tom Pope <thompope@icloud.com>
2019-12-10 16:31:15 -0800

    Begin implementing proxy setup

---
+++ README.md
@@ -0,0 +1,35 @@
+# Project Name
+
+> proxy-tom
+
+## Related Projects
+
+  - https://github.com/hrr42-fec5/service-allen
+  - https://github.com/hrr42-fec5/service-camryn
+  - https://github.com/hrr42-fec5/service-tom
+
+## Table of Contents
+
+1. [Usage](#Usage)
+1. [Requirements](#requirements)
+1. [Development](#development)
+
+## Usage
+
+> Some usage instructions
+
+## Requirements
+
+- Node 6.13.0
+- etc
+
+## Development
+
+### Installing Dependencies
+
+From within the root directory:
+
+```sh
+npm install -g webpack
+npm install
+```
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+
+module.exports = {
+  mode: "production",
+  entry: {
+
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['babel-preset-env', 'babel-preset-react']
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+
+
+    ////////////////////////////////////
+    // BELOW THIS LINE IS IN PROGRESS //
+    ////////////////////////////////////
+
+
+    alias: {
+      // something with styled components?
+      // "module": path.resolve(__dirname, "app/third/module.js")
+    }
+  },
+  context: __dirname, // string (absolute path!)
+  // the home directory for webpack
+  // the entry and module.rules.loader option
+  //   is resolved relative to this directory
+  target: "web",
+  devServer: {
+    proxy: {
+      '/api': 'http://localhost:3000'
+    },
+    // contentBase: path.join(__dirname, 'public'),
+    // compress: true, // enable gzip compression
+    // historyApiFallback: true, // true for index.html upon 404, object for multiple paths
+    // hot: true, // hot module replacement. Depends on HotModuleReplacementPlugin
+    // https: false, // true for self-signed, object for cert authority
+    // noInfo: true // only errors & warns on hot reload
+  }
+}
